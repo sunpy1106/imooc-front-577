7e240453bd74c6dafdcd389f1f66e0f11e2b8a7e
sunpy1106 <sunpy1106@qq.com>
2023-02-26 11:29:21 +0800

    add vueuse

--- src/utils/flexable.js
+++ src/utils/flexable.js
@@ -1,6 +1,8 @@
 import { computed } from 'vue'
 import { PC_DEVICE_WIDTH } from "../constants"
+import { useWindowSize } from '@vueuse/core'
 
+const {width} = useWindowSize()
 /*
 判断当前是否是移动设备
 判断依据： 当前屏幕宽度是否小于指定宽度（1280）
@@ -8,6 +10,6 @@ import { PC_DEVICE_WIDTH } from "../constants"
 
 
 export const isMobileTerminal = computed(() => {
-    console.log("the width:" + document.documentElement.clientWidth)
-    return document.documentElement.clientWidth < PC_DEVICE_WIDTH;
+    console.log("the width:" + width)
+    return width.value < PC_DEVICE_WIDTH;
 })
