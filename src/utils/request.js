f61db382c2a6814233e4d371074b40e92b401d68
sunpy1106 <sunpy1106@qq.com>
2023-02-27 23:17:39 +0800

    add server.proxy and env

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,9 +1,20 @@
 import axios from 'axios'
 
+
+console.log(import.meta.env.VITE_BASE_API)
 const service = axios.create({
-    baseURL: 'https://api.imooc-front.lgdsunday.club/api',
+    baseURL: '/api',
     timeout: 5000
 
 })
+
+service.interceptors.request.use(
+    config => {
+      // 添加 icode
+      config.headers.icode = 'C98AFF9965A0BE4C'
+      // 必须返回 config
+      return config 
+    }
+  )
 
 export default service
